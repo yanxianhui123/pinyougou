ac5f8552746bda6a8f74091bf4315a3e080aac14
yxh <1418053426@qq.com>
2019-08-23 11:25:59 +0800

    pinyougou项目测试2

---
+++ pinyougou-page-service/src/main/java/com/pinyougou/page/service/impl/PageListener.java
@@ -0,0 +1,29 @@
+package com.pinyougou.page.service.impl;
+
+import com.pinyougou.page.service.ItemPageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+@Component
+public class PageListener implements MessageListener {
+    @Autowired
+    private ItemPageService itemPageService;
+
+    @Override
+    public void onMessage(Message message) {
+        TextMessage textMessage = (TextMessage) message;
+        try {
+            String text = textMessage.getText();
+            System.out.println("收到消息"+text);
+            boolean b = itemPageService.genItemHtml(Long.parseLong(text));
+            System.out.println(b);
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+}
